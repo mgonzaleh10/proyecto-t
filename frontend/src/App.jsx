473e8fc2ad7b8749c47087deebee43e330f476dc
Moises Caceres <m.caceres2565@gmail.com>
2025-07-15 15:22:26 -0400

    detalles que no cambian nada.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import './App.css';  
 import { Routes, Route, Link } from 'react-router-dom'
 import Home from './pages/Home'
 import TurnosPage from './pages/TurnosPage'
