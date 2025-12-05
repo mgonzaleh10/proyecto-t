8b655a58bfac83a2a732dbd1b10b0c1d52694d0b
Moises Caceres <m.caceres2565@gmail.com>
2025-12-05 14:00:27 -0300

    ajuste estilos en 3 paginas

--- frontend/src/components/BKNav.jsx
+++ frontend/src/components/BKNav.jsx
@@ -10,12 +10,12 @@ export default function BKNav() {
           <NavLink to="/" end className="pill">Home</NavLink>
           <NavLink to="/usuarios" className="pill">Crews</NavLink>
           <NavLink to="/disponibilidades" className="pill">Disponibilidades</NavLink>
-          <NavLink to="/planilla-manual" className="pill">Planilla Manual</NavLink>
-          <NavLink to="/planilla" className="pill">Planilla</NavLink>
-          <NavLink to="/intercambio" className="pill">Intercambio</NavLink>
+          <NavLink to="/planilla-manual" className="pill">Planilla de Turnos</NavLink>
+          <NavLink to="/planilla" className="pill">Planilla de Turnos 2</NavLink>
+          <NavLink to="/intercambio" className="pill">Intercambio de Turnos</NavLink>
           <NavLink to="/beneficios" className="pill">Beneficios</NavLink>
           <NavLink to="/licencias" className="pill">Licencias</NavLink>
-          <NavLink to="/resumen" className="pill">Resumen</NavLink>
+          <NavLink to="/resumen" className="pill">Resumen y Métricas</NavLink>
           <NavLink to="/horarios" className="pill">Horarios</NavLink>
         </div>
       </div>
