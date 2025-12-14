e36d241bc42544ab70e41a0bb870c4e0261e1fe1
Moises Caceres <m.caceres2565@gmail.com>
2025-12-14 01:20:34 -0300

    cc

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,7 +1,7 @@
 import axios from 'axios'; // Importo axios para comunicarme con el backend
 
 // Tomamos la URL desde variable de entorno (React solo lee las que empiezan con REACT_APP_)
-const API_BASE_URL = process.env.REACT_APP_API_URL || '146.83.198.35:1548';
+const API_BASE_URL = process.env.REACT_APP_API_URL || '146.83.198.35:1549';
 
 const client = axios.create({
   baseURL: API_BASE_URL,
